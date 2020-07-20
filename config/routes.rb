953b3b1ecdc3c6bb6a3e737b6f810ffa1007381d
Thiago Gonçalves Garcia <tggarciah@gmail.com>
2020-07-20 07:48:50 -0300

    Código da terceira aula

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   #get "produtos", to: "produtos#index"
   root to: "produtos#index"
+  get "produtos/new", to: "produtos#new"
+  post "produtos", to: "produtos#create"
 end
